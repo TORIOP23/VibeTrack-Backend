1624f62eed3a6a1b38e8969ced555b86c25ec6c8
Pham Xuan Truong <phamxuantruong843@gmail.com>
2024-11-15 00:30:34 +0700

    Init Spring Boot

---
+++ src/main/java/com/vibetrack/vibetrackbackend/VibeTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vibetrack.vibetrackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeTrackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibeTrackBackendApplication.class, args);
+    }
+
+}
