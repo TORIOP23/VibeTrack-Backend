c6ef5d58c99b82a5d12eb57cae5792c8debddd1d
TORIOP23 <vinhtran23022003@gmail.com>
2024-12-03 22:52:23 +0700

    fix: cors bug

--- identity/src/main/java/com/vibetrack/identity/config/SecurityConfig.java
+++ identity/src/main/java/com/vibetrack/identity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.vibetrack.identity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,6 +31,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors(Customizer.withDefaults());
         httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(PUBLIC_ENDPOINTS)
                 .permitAll()
                 .anyRequest()
