2ae38b9aecec4353a67f89ba6c9df9ed44ccf5b2
TORIOP23 <vinhtran23022003@gmail.com>
2024-12-08 18:54:37 +0700

    feat: add url for song

--- aurora/src/main/java/com/vibetrack/aurora/service/AlbumService.java
+++ aurora/src/main/java/com/vibetrack/aurora/service/AlbumService.java
@@ -12,6 +12,7 @@ import com.vibetrack.aurora.mapper.AlbumMapper;
 import com.vibetrack.aurora.repository.AlbumRepository;
 import com.vibetrack.aurora.repository.SongRepository;
 import com.vibetrack.aurora.repository.TrackRepository;
+import com.vibetrack.aurora.util.ApplicationInit;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -60,6 +61,7 @@ public class AlbumService {
     }
 
     private SongResponse toSongResponse(Song song) {
+        String url = "http://" + ApplicationInit.ipAddress + ":9002/aurora/audio/" + song.getSongId() + ".mp3";
         return SongResponse.builder()
                 .songId(song.getSongId())
                 .songName(song.getSongName())
@@ -68,6 +70,7 @@ public class AlbumService {
                 .popularity(song.getPopularity())
                 .explicit(song.getExplicit())
                 .songType(song.getSongType())
+                .url(url)
                 .build();
     }
 }
--- aurora/src/main/java/com/vibetrack/aurora/util/ApplicationInit.java
+++ aurora/src/main/java/com/vibetrack/aurora/util/ApplicationInit.java
@@ -1,7 +1,25 @@
 package com.vibetrack.aurora.util;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Component
+@Slf4j
 public class ApplicationInit {
+
+    public static String ipAddress;
+
+    @PostConstruct
+    public void init() {
+        try {
+            ipAddress = InetAddress.getLocalHost().getHostAddress();
+            log.info("IP address: {}", ipAddress);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException("Failed to get IP address", e);
+        }
+    }
 }
